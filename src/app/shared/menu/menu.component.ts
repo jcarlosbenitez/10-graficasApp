695b2ee65e9414ffb55ab7e49086d18e3d0c98b4
Carlos Benitez <j.carlos.benitez91@gmail.com>
2022-07-06 00:39:25 -0500

    10-graficas

---
+++ src/app/shared/menu/menu.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+interface MenuItem {
+  ruta: string;
+  texto: string;
+}
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
+})
+export class MenuComponent {
+  menus: MenuItem[] = [
+    {
+      ruta: '/graficas/barra',
+      texto: 'Barra',
+    },
+    {
+      ruta: '/graficas/barra-doble',
+      texto: 'Barra Doble',
+    },
+    {
+      ruta: '/graficas/dona',
+      texto: 'Dona',
+    },
+    {
+      ruta: '/graficas/dona-http',
+      texto: 'Dona Http',
+    },
+  ];
+}
